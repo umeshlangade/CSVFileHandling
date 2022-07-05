48f067928b4267ebbd6fafcd20b2fa2fa085085a
Umesh Langade <umeshlangade5@gmail.com>
2022-07-05 23:05:37 +0530

    PUSH

---
+++ ReadingCSVFiles.py
@@ -0,0 +1,18 @@
+import csv
+
+filename = "C:/Users/umesh/Downloads/bigBasketProductList/BigBasket Products.csv"
+
+fields = []
+rows = []
+
+with open(filename,'r',encoding="utf-8") as csvfile:
+    csv_reader = csv.reader(csvfile)
+    # creating a csv reader object
+    fields = next(csv_reader)
+    # extracting each data row one by one
+    for row in csv_reader:
+        rows.append(row)
+
+    # get total number of rows
+    print(f'total no of rows {csv_reader.line_num}')
+    print(rows[:5])
